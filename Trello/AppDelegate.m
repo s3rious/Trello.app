7979d328a21b1c50234ec989dc778ef587d61e1b
Arvid Andersson <arvid.andersson@oktavilla.se>
2013-05-02 23:00:45 +0200

    Open external links in browser
    If a clicked links host doesn't match www.trello.com or trello.com it
    will be opened with the default browser.

--- Trello/AppDelegate.m
+++ Trello/AppDelegate.m
@@ -19,9 +19,30 @@
     NSURL *url = [NSURL URLWithString:urlAddress];
     NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
     
+    [webview setPolicyDelegate:self];
+    [webview setUIDelegate:self];
+
     [[webview mainFrame] loadRequest:requestObj];
 }
 
 
+- (void)webView:(WebView *)webView
+decidePolicyForNavigationAction:(NSDictionary *)actionInformation
+        request:(NSURLRequest *)request frame:(WebFrame *)frame
+decisionListener:(id < WebPolicyDecisionListener >)listener
+{
+    NSString *host = [[request URL] host];
+    if ([host isNotEqualTo:@"www.trello.com"] && [host isNotEqualTo:@"trello.com"]) {
+        [[NSWorkspace sharedWorkspace] openURL:[request URL]];
+    } else {
+        [listener use];
+    }
+}
+
+- (WebView *)webView:(WebView *)sender createWebViewWithRequest:(NSURLRequest *)request
+{
+    [[sender mainFrame] loadRequest:request];
+    return sender;
+}
 
 @end
